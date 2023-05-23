1863b361a8b942b110ea332b447ea4fb3ae2981b
vanor-cardozo <vanor.fc@gmail.com>
2023-05-23 13:59:20 -0300

    feat: Adicionando fonte personalizada

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
 import { ThemeProvider } from "styled-components";
-import theme from "@theme/index";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import theme from "./src/theme";
+
 import { Groups } from "@screens/Groups";
+import { ActivityIndicator } from "react-native";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }

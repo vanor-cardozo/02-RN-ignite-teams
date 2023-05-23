2954759266c77088e601f76f96d4754422c09e09
vanor-cardozo <vanor.fc@gmail.com>
2023-05-23 18:15:11 -0300

    feat: Componente List Empty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,15 +1,32 @@
+import { useState } from "react";
+
 import { Header } from "@components/Header";
-import { Container, Title } from "./styles";
+import { Container } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
+import { FlatList } from "react-native";
+import { ListEmpty } from "@components/ListEmpty";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([
+    // "Galera da Rocket",
+    // "Amigos",
+  ]);
+
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
 
-      <GroupCard title="Galera do Ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Cadastre sua primeira turma!" />
+        )}
+      />
     </Container>
   );
 }

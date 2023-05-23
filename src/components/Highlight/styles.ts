a829fbcca51220aeab59e0c677c615a4f5756c2a
vanor-cardozo <vanor.fc@gmail.com>
2023-05-23 17:52:30 -0300

    feat: Componente GroupCard

--- src/components/Highlight/styles.ts
+++ src/components/Highlight/styles.ts
@@ -8,7 +8,7 @@ export const Container = styled.View`
 export const Title = styled.Text`
   text-align: center;
 
-  font-size: ${({ theme }) => theme.FONT_SIZE.XL};
+  font-size: ${({ theme }) => theme.FONT_SIZE.XL}px;
   font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
   color: ${({ theme }) => theme.COLORS.WHITE};
 `;
@@ -16,7 +16,7 @@ export const Title = styled.Text`
 export const SubTitle = styled.Text`
   text-align: center;
 
-  font-size: ${({ theme }) => theme.FONT_SIZE.MD};
+  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
   font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
   color: ${({ theme }) => theme.COLORS.GRAY_300};
 `;

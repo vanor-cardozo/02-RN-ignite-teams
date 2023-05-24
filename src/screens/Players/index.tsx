092f88d9edb2ad62a17ea6018dea3c39a7c55692
vanor-cardozo <vanor.fc@gmail.com>
2023-05-24 14:14:38 -0300

    feat: Componente ButtonIcon com ícone dinâmico

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,6 +1,9 @@
 import { Header } from "@components/Header";
-import { Container } from "./styles";
 import { Highlight } from "@components/Highlight";
+import { ButtonIcon } from "@components/ButtonIcon";
+import { Input } from "@components/Input";
+
+import { Container, Form } from "./styles";
 
 export function Players() {
   return (
@@ -11,6 +14,12 @@ export function Players() {
         title="Nome da turma"
         subtitle="adicione a galera e separe os times"
       />
+
+      <Form>
+        <Input placeholder="Nome da pessoa" autoCorrect={false} />
+
+        <ButtonIcon icon="add" />
+      </Form>
     </Container>
   );
 }

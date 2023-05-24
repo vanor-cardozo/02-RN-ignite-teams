8136f4f95d83dd649422479e2966bcf5ae969766
vanor-cardozo <vanor.fc@gmail.com>
2023-05-24 11:59:14 -0300

    feat: Criando interface NewGroup

---
+++ src/screens/NewGroup/index.tsx
@@ -0,0 +1,22 @@
+import { Header } from "@components/Header";
+import { Container, Content, Icon } from "./styles";
+import { Highlight } from "@components/Highlight";
+import { Button } from "@components/Button";
+import { Input } from "@components/Input";
+
+export function NewGroup() {
+  return (
+    <Container>
+      <Header showBackButton />
+      <Content>
+        <Icon />
+        <Highlight
+          title="Nova Turma"
+          subtitle="crie uma turma para adicionar pessoas"
+        />
+        <Input placeholder="Nome da turma" />
+        <Button title="Criar" style={{ marginTop: 20 }} />
+      </Content>
+    </Container>
+  );
+}

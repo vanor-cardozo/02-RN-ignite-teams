43a27f43b5632d856f4b3120f15d26ac6ab50745
vanor-cardozo <vanor.fc@gmail.com>
2023-05-24 16:52:59 -0300

    feat: Implementando navegação pela aplicação

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -7,12 +7,15 @@ import { GroupCard } from "@components/GroupCard";
 import { FlatList } from "react-native";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
+import { useNavigation } from "@react-navigation/native";
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>([
-    // "Galera da Rocket",
-    // "Amigos",
-  ]);
+  const [groups, setGroups] = useState<string[]>([]);
+  const navigation = useNavigation();
+
+  function handleNewGroup() {
+    navigation.navigate("new");
+  }
 
   return (
     <Container>
@@ -29,7 +32,7 @@ export function Groups() {
         )}
       />
 
-      <Button title="Criar nova turma" />
+      <Button title="Criar nova turma" onPress={handleNewGroup} />
     </Container>
   );
 }

3fce44c0b9b1f596a1e4d614d4e3f27d6578f41e
vanor-cardozo <vanor.fc@gmail.com>
2023-05-24 11:22:11 -0300

    feat: Criando componente Button

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -6,6 +6,7 @@ import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
 import { FlatList } from "react-native";
 import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([
@@ -27,6 +28,8 @@ export function Groups() {
           <ListEmpty message="Cadastre sua primeira turma!" />
         )}
       />
+
+      <Button title="Criar nova turma" />
     </Container>
   );
 }
